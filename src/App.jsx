0e9feae9e9d2120d5e1a03527f5e224a180c930b
laianribas <laian67a@gmail.com>
2022-01-22 21:12:14 -0300

    commit inicial e criação dos cards

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Exercício React-Redux (Simples)</h1>
+      <div className="linha">
+        <Card title="Card 1" red>
+          x
+        </Card>
+        <Card title="Card 2" green>
+          x
+        </Card>
+      </div>
+      <div className="linha">
+        <Card title="Card 3" blue>
+          y
+        </Card>
+        <Card title="Card 4" purple>
+          y
+        </Card>
+      </div>
+    </div>
+  )
+}
+
+export default App
